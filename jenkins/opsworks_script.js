8b89e1699d557a3a108060c2b3b09b8d7c072754
mpalkhade <manojpalkhade@gmail.com>
2018-07-24 17:27:06 -0700

    Create opsworks_script.js

---
+++ jenkins/opsworks_script.js
@@ -0,0 +1,7 @@
+cd files
+cp //Users/mpalkhade/Downloads/sapana_aws_new.pem sapana_aws_new.pem
+chmod 755 sapana_aws_new.pem
+npm install
+node GigyaStack.js gigya-qa qa
+
+sudo mv set_parameters.env /params/gigya.properties
