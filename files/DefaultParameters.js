ab7124f328f51bd7e28a956716f560ce9feb77bd
Manoj Palkhade <manoj_palkhade@spe.sony.com>
2018-07-24 17:00:41 -0700

    all required files for opsworks POC

---
+++ files/DefaultParameters.js
@@ -0,0 +1,80 @@
+var fs = require('fs');
+
+module.exports = function(env) {
+
+    var customJson = {
+      "opsworks_java": {
+      "jvm_version": 8,
+      "java_app_server_version": 8
+      },
+      "custom_env": {
+        "gigya-qa": {
+          "type": "java",
+          "jar": "B2BPlatformServices-1.0.0.RELEASE.jar"
+        }
+      }
+    }
+
+    var customJsonString = JSON.stringify(customJson) ;
+
+    var settings = {
+        defaultStackParams: {
+            Region: 'us-west-2',
+            ConfigurationManager: {
+                Name: 'Chef',
+                Version: '11.10'
+            },
+            DefaultRootDeviceType: 'ebs',
+            DefaultOs: 'Amazon Linux 2017.09',
+            UseCustomCookbooks: true,
+            CustomCookbooksSource: {
+                Type: 'git',
+                Url: 'https://github.com/ykhati/ChefRepo.git'
+            },
+            UseOpsworksSecurityGroups: true,
+            DefaultSshKeyName: 'sapana_aws_new'
+        },
+
+        /* Layer Default Settings */
+        defaultLayerParams: {
+            Type: 'java-app',
+            CustomRecipes: {
+                Deploy: [ 'java::deploy' ]
+            },
+            AutoAssignElasticIps: false,
+            AutoAssignPublicIps: true,
+            EnableAutoHealing: true,
+            InstallUpdatesOnBoot: true,
+            UseEbsOptimizedInstances: false,
+            CustomJson: customJsonString
+        },
+
+        /* Yogesh - App Default Settings */
+        defaultAppParams: {
+            Type: 'java',
+            AppSource: {
+              Type: 'git',
+              Url: 'https://github.com/ykhati/Opsworks.git',
+              Username: 'ykhati'
+            },
+            EnableSsl: false
+        },
+
+        /* Default Instance Settings */
+        defaultInstanceParams: {
+            Architecture: 'x86_64'
+        }
+    };
+
+    switch (env) {
+        case 'qa':
+            settings.defaultStackParams.DefaultInstanceProfileArn = 'arn:aws:iam::123208278418:instance-profile/aws-opsworks-ec2-role';
+            settings.defaultStackParams.ServiceRoleArn = 'arn:aws:iam::123208278418:role/aws-opsworks-service-role';
+            settings.defaultStackParams.DefaultSubnetId = 'subnet-75c42e29';
+            settings.defaultStackParams.VpcId = 'vpc-a65800dd';
+            settings.defaultLayerParams.CustomSecurityGroupIds = ['sg-f3d038bb'];
+            break;
+    }
+
+    return settings;
+};
